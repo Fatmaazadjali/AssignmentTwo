4e3381824bd9b81aa88f5d069f91b1315993e00d
Fatmazadjali <maathermohammed77@gmail.com>
2023-11-20 14:54:53 +0400

    update

--- AssignmentTwo solution/AssignmentTwo/Program.cs
+++ AssignmentTwo solution/AssignmentTwo/Program.cs
@@ -4,6 +4,15 @@ namespace AssignmentTwo
 {
 	 class Program
 	{
+
+		//static void Data(string name,  string price, string quantity)
+		//{
+
+		//	if (name == "laptop")
+		//	{
+
+		//	}
+		//}
 		static void Main()
 		{
 			Console.WriteLine("Welcome to the Inventory Management System!");
@@ -21,21 +30,37 @@ namespace AssignmentTwo
 				Console.WriteLine("Incorrect username or password!");
 			}
 			string[] options = new string[7];
-			options[0] = "Add a new product";
-			options[1] = "Update product quantity";
-			options[2] = "Display product list";
-			options[3] = "Record sale";
-			options[4] = "Generate product report";
-			options[5] = "Generate sales report";
-			options[6] = "Exit";
-			Console.WriteLine("Select an operation");
+			options[0] = "1.Add a new product";
+			options[1] = "2.Update product quantity";
+			options[2] = "3.Display product list";
+			options[3] = "4.Record sale";
+			options[4] = "5.Generate product report";
+			options[5] = "6.Generate sales report";
+			options[6] = "7.Exit";
+			Console.WriteLine("\nSelect an operation");
+
 
 			//for (string option =  options[0]; option != null;)
 			//{
 			//	Console.WriteLine(option);
 			//}
 
-			Console.WriteLine($"{options[0]}" + $"{options[1]}" + $"{options[2]}" + $"{options[3]}" + $"{options[4]}"+ $"{options[5]}"+ $"{options[6]}");
+			Console.WriteLine($"{options[0]}" + $"\n{options[1]}" + $"\n{options[2]}" + $"\n{options[3]}" + $"\n {options[4]}"+ $"\n {options[5]}"+ $"\n {options[6]}");
+
+			Console.ReadLine();
+
+			String UserChoice = options[0];
+			if (UserChoice == options[0])
+			{
+                Console.WriteLine("Enter the product name:");
+				Console.ReadLine();
+                Console.WriteLine("Enter product price:");
+				Console.ReadLine();
+                Console.WriteLine("Enter initial quantity:");
+				Console.ReadLine();
+                Console.WriteLine("Product added successfully!");
+            }
+			
 
 
 
